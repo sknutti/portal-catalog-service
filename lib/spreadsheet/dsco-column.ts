60b76ce570a783d788f9cd40babeaafabeda3e45
Vultix <mraidangrant@gmail.com>
2020-10-01 13:05:20 -0600

    Fixed image names disappearing when updating spreadsheet

--- lib/spreadsheet/dsco-column.ts
+++ lib/spreadsheet/dsco-column.ts
@@ -150,7 +150,11 @@ export class DscoColumn {
      */
     private makeExtendedValue(rowData: DscoCatalogRow, retailerId: number): Schema$ExtendedValue | undefined {
         let data: any;
-        if (this.type === 'core') {
+
+        if (this.validation.format === 'image') {
+            const [arrName, imgName] = this.imageNames;
+            data = rowData.catalog[arrName].find(img => img.name === imgName)?.source_url;
+        } else if (this.type === 'core') {
             data = rowData.catalog[this.fieldName];
         } else if (this.type === 'extended') {
             data = rowData.catalog.extended_attributes?.[retailerId]?.[this.fieldName];
