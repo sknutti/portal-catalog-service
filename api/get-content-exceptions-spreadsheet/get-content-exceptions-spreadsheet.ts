c411a4beab2900939a98c399cf534a55c320b8ea
Eric Miller <emiller@commercehub.com>
2021-11-15 09:57:47 -0600

    CCR-86: branching out changes needed to add in-cell comments

--- api/get-content-exceptions-spreadsheet/get-content-exceptions-spreadsheet.ts
+++ api/get-content-exceptions-spreadsheet/get-content-exceptions-spreadsheet.ts
@@ -3,12 +3,13 @@ import { MissingRequiredFieldError, UnauthorizedError } from '@dsco/ts-models';
 import { getLeoAuthUserTable } from '@lib/environment';
 import { DscoCatalogRow, DscoSpreadsheet, generateDscoSpreadsheet } from '@lib/spreadsheet';
 import { xlsxFromDsco } from '@lib/spreadsheet/physical-spreadsheet/xlsx-from-dsco';
-import { catalogItemSearch, gzipAsync } from '@lib/utils';
+import { catalogExceptionsItemSearch, gzipAsync } from '@lib/utils';
 import { GenerateContentExceptionsSpreadsheetRequest } from './get-content-exceptions-spreadsheet.request';
 import { XlsxSpreadsheet } from '@lib/spreadsheet';
 import * as XLSX from '@sheet/image';
 import { CellObject, Comments, DataValidation, Style, utils, WorkSheet } from '@sheet/image';
 import { DscoColumn } from '@lib/spreadsheet/dsco-column';
+import { CoreCatalog } from '@lib/core-catalog';
 
 export const getContentExceptionsSpreadsheet = apiWrapper<GenerateContentExceptionsSpreadsheetRequest>(
     async (event) => {
@@ -30,34 +31,42 @@ export const getContentExceptionsSpreadsheet = apiWrapper<GenerateContentExcepti
         //     return new UnauthorizedError();
         // }
 
-        // const supplierId = user.accountId;
-        // const { retailerId, categoryPath } = event.body;
-
-        // const catalogItems = await catalogItemSearch(supplierId, retailerId, categoryPath);
-
         // const spreadsheet = await generateDscoSpreadsheet(supplierId, retailerId, categoryPath);
 
         // if (!(spreadsheet instanceof DscoSpreadsheet)) {
         //     return spreadsheet;
         // }
 
-        // for (const catalog of catalogItems) {
-        //     // Populate the spreadsheet with all of their catalog items
-        //     spreadsheet.addCatalogRow(new DscoCatalogRow(catalog, false, false));
-        // }
+        const supplierId = 654321; // Placeholder, replace with user.accountId ?
+        const categoryPath = event.body.categoryPath;
+        const retailerId = 123456; // Placeholder, replace with event.body.retailerId
 
-        const spreadsheet = new DscoSpreadsheet('ccr test');
-        const myCol = new DscoColumn('sku', 'this will be a description', 'core', {
-            required: 'none',
-        });
-        spreadsheet.addColumn(myCol);
-        const workbook = xlsxFromDsco(spreadsheet, 123456);
+        // TODO CCR below function call returns dummy values, function call will likely need to take 3 parameters
+        const catalogExceptionItems: CoreCatalog[] = await catalogExceptionsItemSearch(); //supplierId, retailerId, categoryPath);
 
-        console.log(JSON.stringify(workbook.toBuffer()));
+        const spreadsheet = new DscoSpreadsheet(`Catalog Exceptions ${categoryPath}`);
+
+        // Populate spreadsheet with rows of data
+        for (const catalogItem of catalogExceptionItems) {
+            spreadsheet.addCatalogRow(new DscoCatalogRow(catalogItem, false, false));
+        }
+
+        // Columns should be added after rows
+        for (const colName of ['sku', 'long_description']) {
+            spreadsheet.addColumn(
+                new DscoColumn(colName, 'this will be a description', 'core', {
+                    required: 'none',
+                }),
+            );
+        }
+
+        const workbook = xlsxFromDsco(spreadsheet, retailerId);
+
+        //workbook.toFile(); // TODO test line only take out before committing
 
         return {
             success: true,
-            gzippedFile: '[0]', //await gzipAsync(workbook.toBuffer()),
+            gzippedFile: await gzipAsync(workbook.toBuffer()),
         };
     },
 );
