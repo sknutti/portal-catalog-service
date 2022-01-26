5fd5ccd42b8194f41b321744be6af0293ef4c1e2
edavid <edavid@commercehub.com>
2022-01-26 14:27:04 -0500

    updated interface to current api

--- lib/utils/item-exceptions-search.request.ts
+++ lib/utils/item-exceptions-search.request.ts
@@ -8,15 +8,23 @@ export interface ItemExceptionSearchBody {
     version: 1;
     pageSize?: number;
     paginationKey?: null | number[];
+    fullDetail?: boolean;
 }
 
+interface ItemExceptionSearchResponseTotal {
+    value: number;
+    relation: string;
+}
+//TODO: This should be imported to match item/api/exception response
 export interface ItemExceptionSearchResponse extends DsResponse {
-    items: CoreCatalog[];
-    config: any; // an object, echoes header information back
-    headers: any; // an object, details about the execution such as datetime and connection status
-    request: any; // cannot check this value with JSON.stringify...
-    status: number; // http status, so should usually be 200
-    statusText: string; // http status, if status is 200, this is 'OK'
+    items: CoreCatalog[] | number[];
+    duration: number;
+    returned: number;
+    total: ItemExceptionSearchResponseTotal;
+    paginationKey: number[];
+    pageSize: number;
+    hitRange: any;
+    pageNumber?: number;
 }
 
 export class ItemExceptionSearchRequest extends DsRequest<
