82cbe53de27da06107dc8184e0cf763e3aadfa41
Ben Broide <bbroide@mac-bbroide.local>
2022-02-17 12:19:04 -0800

    added bucket for migrate retails models

--- lib/environment.ts
+++ lib/environment.ts
@@ -58,4 +58,5 @@ export function setupEnvironmentForRunningLocally(env: Exclude<DscoEnv, 'dev'>):
     process.env.AUTH_USER_TABLE = vars.authTable;
     process.env.GEARMAN_HOST = `gearman.${env === 'prod' ? 'local' : env}`;
     process.env.S3_BUCKET = `portal-catalog-${env}`;
+    process.env.S3_BUCKET_ITEMS_MIGRATE_RETAIL_MODEL = `items-migrate-retail-models-${env}`;
 }
