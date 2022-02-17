cbafa96a8c515ff285574b57e503b91fbb17ba23
David Cross <dcross@MAC-DCROSS2.local>
2022-02-17 18:03:44 -0500

    CCR-207: missed a rename

--- api/index.ts
+++ api/index.ts
@@ -3,4 +3,4 @@ export * from './generate-category-spreadsheet/generate-category-spreadsheet.req
 export * from './get-assortments/get-assortments.request';
 export * from './get-category-spreadsheet-upload-url/get-category-spreadsheet-upload-url.request';
 export * from './get-content-exceptions-spreadsheet/get-content-exceptions-spreadsheet.request';
-export * from './get-upload-items-spreadsheet-migrate-retail-models/get-upload-items-spreadsheet-migrate-retail-models.request';
+export * from './get-channel-overrides-spreadsheet-upload-url/get-channel-overrides-spreadsheet-upload-url.request';
