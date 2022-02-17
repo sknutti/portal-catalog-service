70f9eeb8266162c679b49bb45b1e62406bee6fc2
Ben Broide <bbroide@ip-192-168-68-151.ec2.internal>
2022-02-17 14:04:41 -0800

    format code

--- api/get-upload-items-spreadsheet-migrate-retail-models/get-upload-items-spreadsheet-migrate-retail-models.ts
+++ api/get-upload-items-spreadsheet-migrate-retail-models/get-upload-items-spreadsheet-migrate-retail-models.ts
@@ -1,33 +1,38 @@
 import { apiWrapper, getUser } from '@dsco/service-utils';
 import { MissingRequiredFieldError, UnauthorizedError } from '@dsco/ts-models';
 import { getIsRunningLocally, getLeoAuthUserTable } from '@lib/environment';
-import { ItemsUploadMigrateRetailModelSpreadsheetS3Metadata, createCatalogItemS3UploadPath, getSignedS3UploadUrlMigrateRetailModel } from '@lib/s3';
+import {
+    ItemsUploadMigrateRetailModelSpreadsheetS3Metadata,
+    createCatalogItemS3UploadPath,
+    getSignedS3UploadUrlMigrateRetailModel,
+} from '@lib/s3';
 import { GetUploadItemsSpreadsheetMigrateRetailModelsRequest } from './get-upload-items-spreadsheet-migrate-retail-models.request';
 
-export const getUploadItemsSpreadsheetMigrateRetailModelsUrl = apiWrapper<GetUploadItemsSpreadsheetMigrateRetailModelsRequest>(async (event) => {
-    if (!event.body.retailerId) {
-        return new MissingRequiredFieldError('retailerId');
-    }
-    
-    const user = await getUser(event.requestContext, getLeoAuthUserTable());
+export const getUploadItemsSpreadsheetMigrateRetailModelsUrl =
+    apiWrapper<GetUploadItemsSpreadsheetMigrateRetailModelsRequest>(async (event) => {
+        if (!event.body.retailerId) {
+            return new MissingRequiredFieldError('retailerId');
+        }
 
-    // Must be logged in
-    if (!user?.accountId || !user.retailerIds?.includes(event.body.retailerId)) {
-        return new UnauthorizedError();
-    }
+        const user = await getUser(event.requestContext, getLeoAuthUserTable());
 
-    const { retailerId, skippedRowIndexes } = event.body;
+        // Must be logged in
+        if (!user?.accountId || !user.retailerIds?.includes(event.body.retailerId)) {
+            return new UnauthorizedError();
+        }
 
-    const uploadMeta: ItemsUploadMigrateRetailModelSpreadsheetS3Metadata = {
-        skipped_row_indexes: skippedRowIndexes?.join(','),
-        is_local_test: getIsRunningLocally() ? 'true' : undefined,
-    };
+        const { retailerId, skippedRowIndexes } = event.body;
 
-    return {
-        success: true,
-        uploadUrl: await getSignedS3UploadUrlMigrateRetailModel(
-            createCatalogItemS3UploadPath(user.accountId, retailerId, user.userId,'spreadsheets'),
-            uploadMeta,
-        ),
-    };
-});
+        const uploadMeta: ItemsUploadMigrateRetailModelSpreadsheetS3Metadata = {
+            skipped_row_indexes: skippedRowIndexes?.join(','),
+            is_local_test: getIsRunningLocally() ? 'true' : undefined,
+        };
+
+        return {
+            success: true,
+            uploadUrl: await getSignedS3UploadUrlMigrateRetailModel(
+                createCatalogItemS3UploadPath(user.accountId, retailerId, user.userId, 'spreadsheets'),
+                uploadMeta,
+            ),
+        };
+    });
