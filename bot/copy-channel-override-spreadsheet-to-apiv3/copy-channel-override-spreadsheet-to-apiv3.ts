8545b7d52d643237e465f349c5e4ddd75ef1f418
Trey Fenner <tfenner@commercehub.com>
2022-03-03 17:41:47 -0600

    Clean up comments

--- bot/copy-channel-override-spreadsheet-to-apiv3/copy-channel-override-spreadsheet-to-apiv3.ts
+++ bot/copy-channel-override-spreadsheet-to-apiv3/copy-channel-override-spreadsheet-to-apiv3.ts
@@ -54,21 +54,3 @@ export async function copyChannelOverrideSpreadsheetToApiV3(
 
     await s3.copyObject(copyParams).promise();
 }
-
-// {
-//     "s3SchemaVersion": "1.0",
-//     "configurationId": "8e7e4828-359c-4b07-9bb5-f2619607c60f",
-//     "bucket": {
-//         "name": "portal-catalog-test",
-//         "ownerIdentity": {
-//             "principalId": "A2ZXFL2I37U6D"
-//         },
-//         "arn": "arn:aws:s3:::portal-catalog-test"
-//     },
-//     "object": {
-//         "key": "channel-overrides/uploads/1000012301/26365/spreadsheets/90c28f1f-7cfe-4504-b26a-6a84ba8f9b88",
-//         "size": 9501,
-//         "eTag": "7bdfa8dae7a20b50f0569d4330edb966",
-//         "sequencer": "0062201B2ED3EA991E"
-//     }
-// }
