7e9218f299640c57ea7ea01c5498096e55b77f9a
Eric Miller <emiller@commercehub.com>
2022-04-06 12:48:47 -0500

    PMK-294: rerouting broken index links

--- api/index.ts
+++ api/index.ts
@@ -6,4 +6,4 @@ export * from './get-content-exceptions-spreadsheet/get-content-exceptions-sprea
 export * from './get-channel-overrides-upload-url/get-channel-overrides-upload-url.request';
 export * from './get-all-assortments/get-all-assortments.request';
 
-export * from '../lib/utils/channel-override-write';
+export * as OverrideUtils from '../lib/utils/channel-override-write';
